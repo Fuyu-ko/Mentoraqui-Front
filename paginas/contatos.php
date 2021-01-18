2b6a335d4b8a77fff0ae1f5da263f9562e1874b4
Fuyu-ko <55238889+Gabriel-Barboza@users.noreply.github.com>
2021-01-17 21:44:09 -0300

    Update 7
    - Adição dos contatos

--- paginas/contatos.php
+++ paginas/contatos.php
@@ -14,27 +14,119 @@
 				<td><div class="cartaoContato">
 					<table>
 						<tr>
-							<td colspan="2"><h3>Ana Paula Schran de Almeida</h3></td>
+							<td colspan="2"><h4>Ana Paula Schran de Almeida</h4></td>
 						</tr>
 						<tr>
-							<td><img src="../imagens/inIcon.png"></td>
-							<td></td>
+							<td><img class="conIcon" src="../imagens/inIcon.png"></td>
+							<td><h5 class="contTxt"><a href="https://linkedin.com/in/anaschran">linkedin.com/in/anaschran</a></h5></td>
 						</tr>
 						<tr>
-							<td><img src="../imagens/emailIcon.png"></td>
-							<td></td>
+							<td><img class="conIcon" src="../imagens/emailIcon.png"></td>
+							<td><h5 class="contTxt">anaschran28@gmail.com</h5></td>
 						</tr>
 						<tr>
-							<td><img src="../imagens/celIcon.png"></td>
-							<td></td>
+							<td><img class="conIcon" src="../imagens/celIcon.png"></td>
+							<td><h5 class="contTxt">(41) 99501-3352</h5></td>
 						</tr>
 					</table>
 				</div></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
+				<td><div class="cartaoContato">
+					<table>
+						<tr>
+							<td colspan="2"><h4>Diego de Castro Rodrigues</h4></td>							
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/inIcon.png"></td>
+							<td><h5 class="contTxt"><a href="https://linkedin.com/in/diegocastro-r">linkedin.com/in/diegocastro-r</a></h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/emailIcon.png"></td>
+							<td><h5 class="contTxt">diegocrodrigues.dev@gmail.com</h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/celIcon.png"></td>
+							<td><h5 class="contTxt">(87) 98144-7741</h5></td>
+						</tr>
+					</table>
+				</div></td>
+				<td><div class="cartaoContato">
+					<table>
+						<tr>
+							<td colspan="2"><h4>Gabriel Ernesto Barboza Pereira</h4></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/inIcon.png"></td>
+							<td><h5 class="contTxt"><a href="https://linkedin.com/in/gabriel-ernesto-barboza-pereira-6933621a2">linkedin.com/in/gabriel-ernesto-barboza-pereira-6933621a2</a></h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/emailIcon.png"></td>
+							<td><h5 class="contTxt">gabriel.ernesto2001@gmail.com</h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/celIcon.png"></td>
+							<td><h5 class="contTxt">(41) 99702-7275</h5></td>
+						</tr>
+					</table>
+				</div></td>
+			</tr>
+			<tr>
+				<td><div class="cartaoContato">
+					<table>
+						<tr>
+							<td colspan="2"><h4>Lethicia de Moura Silva</h4></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/inIcon.png"></td>
+							<td><h5 class="contTxt"><a href=""></a></h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/emailIcon.png"></td>
+							<td><h5 class="contTxt">lethmouras@gmail.com</h5></td>			
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/celIcon.png"></td>
+							<td><h5 class="contTxt">(21) 99069-9381</h5></td>
+						</tr>
+					</table>
+				</div></td>
+				<td><div class="cartaoContato">
+					<table>
+						<tr>
+							<td colspan="2"><h4>Scarlet Cristina de Araújo</h4></td>							
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/inIcon.png"></td>
+							<td><h5 class="contTxt"><a href="https://linkedin.com/in/scarlet-ara%C3%BAjo-61160852">linkedin.com/in/scarlet-ara%C3%BAjo-61160852</a></h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/emailIcon.png"></td>
+							<td><h5 class="contTxt">scarlet.araujo@gmail.com</h5></td>							
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/celIcon.png"></td>
+							<td><h5 class="contTxt">(11) 95425-5526</h5></td>
+						</tr>
+					</table>
+				</div></td>
+				<td><div class="cartaoContato">
+					<table>
+						<tr>
+							<td colspan="2"><h4>Simone Perez Miquelin</h4></td>							
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/inIcon.png"></td>
+							<td><h5 class="contTxt"><a href="https://linkedin.com/in/simone-miquelin">linkedin.com/in/simone-miquelin</a></h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/emailIcon.png"></td>
+							<td><h5 class="contTxt">Simonemiquelin@yahoo.com.br</h5></td>
+						</tr>
+						<tr>
+							<td><img class="conIcon" src="../imagens/celIcon.png"></td>
+							<td><h5 class="contTxt">(11) 96665-1246</h5></td>							
+						</tr>
+					</table>
+				</div></td>
 			</tr>
 		</table>
 	</div>
